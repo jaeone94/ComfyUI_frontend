3d520cfbdcad3c4aa8bf515a2cf1fed382421ed6
Austin Mroz <austin@comfy.org>
2025-12-15 17:27:56 -0800

    Set dirty to ensure disconnection circle updates
    Moving the draw operation to the background means that the distance
    check logic was no longer being tracked. since only a single bg draw was
    made.
    
    This is slightly less than ideal, but it's reasonable to expect that the
    user will continually be dragging the mouse when moving links

--- src/lib/litegraph/src/LGraphCanvas.ts
+++ src/lib/litegraph/src/LGraphCanvas.ts
@@ -4770,6 +4770,7 @@ export class LGraphCanvas implements CustomEventDispatcher<LGraphCanvasEventMap>
               }
             )
           }
+          if (renderLink instanceof MovingInputLink) this.setDirty(false, true)
 
           ctx.fillStyle = colour
           ctx.beginPath()
