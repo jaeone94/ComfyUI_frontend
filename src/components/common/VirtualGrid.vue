c1d45a00d1081ac26fc1a9e4b89c7ffc7caf7d5c
GitHub Action <action@github.com>
2026-02-01 06:01:18 +0000

    [automated] Apply ESLint and Oxfmt fixes

--- src/components/common/VirtualGrid.vue
+++ src/components/common/VirtualGrid.vue
@@ -57,8 +57,6 @@ const emit = defineEmits<{
   'approach-end': []
 }>()
 
-
-
 const itemHeight = ref(defaultItemHeight)
 const itemWidth = ref(defaultItemWidth)
 const container = ref<HTMLElement | null>(null)
